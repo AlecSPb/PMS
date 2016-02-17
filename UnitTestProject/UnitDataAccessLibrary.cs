8445ce2c9d7d831e2d382f6fcc038ae964cd3a28
xs.zhou <newlifechou@outlook.com>
2016-02-17 14:37:57 +0800

    单元测试EF的初步使用

---
+++ UnitTestProject/UnitDataAccessLibrary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccessLibrary;
+using System.Linq;
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitDataAccessLibrary
+    {
+        private ProductionManagementModel db;
+        public UnitDataAccessLibrary()
+        {
+            db = new ProductionManagementModel();
+        }
+
+        [TestMethod]
+        public void TestDataAccessOne()
+        {
+            int dataCount = db.View_MainOrders.Count();
+            Assert.IsTrue(dataCount>0);
+        }
+    }
+}
