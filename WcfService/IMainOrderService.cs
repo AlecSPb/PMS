e874e9f105b9dcabadba4a4c705bfc1c7017ab2e
xs.zhou <newlifechou@outlook.com>
2016-02-18 13:35:30 +0800

    完成MainOrderService里的接口契约

--- WcfService/IMainOrderService.cs
+++ WcfService/IMainOrderService.cs
@@ -13,10 +13,40 @@ namespace WcfService
     public interface IMainOrderService
     {
         /// <summary>
-        /// 返回全部的订单
+        /// 返回所有的订单列表
         /// </summary>
         /// <returns></returns>
         [OperationContract]
         List<MainOrder> GetAllMainOrders();
+        /// <summary>
+        /// 按照Id查找订单
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [OperationContract]
+        MainOrder GetMainOrderById(Guid id);
+
+        /// <summary>
+        /// 添加订单
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool AddMainOrder(MainOrder order);
+        /// <summary>
+        /// 更新订单
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool UpdateMainOrder(MainOrder order);
+        /// <summary>
+        /// 删除订单
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool DeleteMainOrder(MainOrder order);
+
     }
 }
