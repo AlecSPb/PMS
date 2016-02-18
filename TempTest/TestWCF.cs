805c6d281e60f8128f92036574d2a3ae0d4d5abb
xs.zhou <newlifechou@outlook.com>
2016-02-18 13:29:56 +0800

    添加一个临时测试项目，测试OK

---
+++ TempTest/TestWCF.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfService;
+
+namespace TempTest
+{
+    class TestWCF
+    {
+        private MainOrderService service;
+        public TestWCF()
+        {
+            service = new MainOrderService();
+        }
+        public void GetAllMainOrders()
+        {
+            service.GetAllMainOrders().ForEach(mo=>Console.WriteLine(mo.ProductName));
+        }
+    }
+}
