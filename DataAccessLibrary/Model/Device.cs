3d36cd8ffaebbdc8469004218f5c17820512cae2
xs.zhou <newlifechou@outlook.com>
2016-02-22 13:55:49 +0800

    添加Device Model

--- DataAccessLibrary/Model/Device.cs
+++ DataAccessLibrary/Model/Device.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLibrary.Model
 {
-    class Device
+    [Table("Device")]
+    public class Device
     {
+        [Key]
+        public Guid DeviceId { get; set; }
+
+        public string DeviceName { get; set; }
+
+        public string DeviceCode { get; set; }
+
+        public int TopTemperature { get; set; }
+
+        public int TopPressure { get; set; }
+
+        public int TopDiameter { get; set; }
+
+        public string Remark { get; set; }
+
     }
 }
--- WcfService/Model/Device.cs
+++ WcfService/Model/Device.cs
@@ -7,5 +7,18 @@ namespace WcfService.Model
 {
     public class Device
     {
+        public Guid DeviceId { get; set; }
+
+        public string DeviceName { get; set; }
+
+        public string DeviceCode { get; set; }
+
+        public int TopTemperature { get; set; }
+
+        public int TopPressure { get; set; }
+
+        public int TopDiameter { get; set; }
+
+        public string Remark { get; set; }
     }
 }
