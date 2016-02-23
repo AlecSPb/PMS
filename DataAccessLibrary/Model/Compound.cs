1593194f1b20797f00dad3e77fd9880c61afd078
xs.zhou <newlifechou@outlook.com>
2016-02-23 09:58:51 +0800

    Finish Compound Model

--- DataAccessLibrary/Model/Compound.cs
+++ DataAccessLibrary/Model/Compound.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLibrary.Model
 {
-    class Compound
+    [Table("Compound")]
+    public class Compound
     {
+        [Key]
+        public Guid CompoundId { get; set; }
+
+        public string CompoundName { get; set; }
+
+        public double Density { get; set; }
+
+        public double  MeltingPoint { get; set; }
+
+        public double BoilingPoint { get; set; }
+
+        public string SpecialProperty { get; set; }
+
+        public DateTime UpdateTime { get; set; }
+
+        public string InformationSource { get; set; }
+
+        public string Remark { get; set; }
+
     }
 }
