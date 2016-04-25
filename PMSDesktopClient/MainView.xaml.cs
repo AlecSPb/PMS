2b5b58f98ffadc9138480402ea34e9b475497077
xs.zhou <newlifechou@outlook.com>
2016-04-25 15:36:49 +0800

    更改主窗体名称

--- PMSDesktopClient/MainView.xaml.cs
+++ PMSDesktopClient/MainView.xaml.cs
@@ -18,9 +18,9 @@ namespace PMSDesktopClient
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : Window
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
         }
---
+++ PMSDesktopClient/WindowManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Developer:xs.zhou@outlook.com
+    CreateTime:2016/4/25 15:30:47
+*/
+namespace PMSDesktopClient
+{
+    public class WindowManager
+    {
+
+    }
+}
