9fb5647af2cb869ee281a2773ad37d7e5911d93f
xs.zhou <newlifechou@outlook.com>
2016-04-25 10:58:38 +0800

    完成Customer的Model

--- PMSDAL/Model/Customer.cs
+++ PMSDAL/Model/Customer.cs
@@ -8,5 +8,13 @@ namespace PMSDAL.Model
 {
     public class Customer
     {
+        public Guid Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string ContractPerson { get; set; }
+        public string CellPhone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Remark { get; set; }
     }
 }
