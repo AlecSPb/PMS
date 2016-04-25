7e7dc6e33eb3941aee8cf9e8011e53ab2eb507c0
xs.zhou <newlifechou@outlook.com>
2016-04-25 16:23:12 +0800

    添加WPFSkin项目

---
+++ WPFValidationRule/RegexRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Developer:xs.zhou@outlook.com
+    CreateTime:2016/4/25 15:59:31
+*/
+namespace WPFValidationRule
+{
+    public class RegexRule
+    {
+        public string Pattern { get; set; }
+
+    }
+}
