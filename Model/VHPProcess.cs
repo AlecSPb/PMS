61c41d1da9a134815ead75fe198a8c715f3b3c40
xs.zhou <newlifechou@outlook.com>
2016-10-09 15:15:51 +0800

    create vhpprocess

---
+++ Model/VHPProcess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class VHPProcess
+    {
+        public Guid ID { get; set; }
+        public string CodeName { get; set; }
+        public string CodeMeaning { get; set; }
+        public string Creator { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
