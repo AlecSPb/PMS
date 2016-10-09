ae0dfec91043a49eeed35d25284b8dc013c3f6f3
xs.zhou <newlifechou@outlook.com>
2016-10-09 15:06:00 +0800

    finish class customer

--- Model/PlanItem.cs
+++ Model/PlanItem.cs
@@ -12,12 +12,13 @@ namespace Model
     public class PlanItem
     {
         public Guid ID { get; set; }
-        public Guid PlanID { get; set; }
-        public Guid OrderID { get; set; }
+        public Guid PlanID { get; set; }//对应的计划
+        public Guid OrderID { get; set; }//对应的订单
         public double CalculationDensity { get; set; }
         public double Thickness { get; set; }
         public int Quantity { get; set; }
         public string MillingRequirement { get; set; }
+        //单片装料重量和所有装料重量可以通过对上面的项目计算出来
 
         public string Creator { get; set; }
         public DateTime CreateDate { get; set; }
