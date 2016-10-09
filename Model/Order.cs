0d4d044398f68ea036f42c5727b9d5734f6917a4
xs.zhou <newlifechou@outlook.com>
2016-10-09 13:36:18 +0800

    create a project to put models

---
+++ Model/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Order
+    {
+        public Guid ID { get; set; }
+        public string CustomerName { get; set; }
+        public string PO { get; set; }
+        public string Composition { get; set; }
+        public string PMIWorkingNumber { get; set; }
+        public string ProductType { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public bool OrderState { get; set; }
+        public int Priority { get; set; }
+    }
+}
