43e926000db5ea74010cbd619d6205373293f80b
xs.zhou <newlifechou@outlook.com>
2016-10-10 09:08:16 +0800

    create materialrequirement

---
+++ Model/MaterialRequirement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class MaterialRequirement
+    {
+        public Guid Id { get; set; }
+        public string Composition { get; set; }
+        public string Purity { get; set; }
+        public double Weight { get; set; }
+        public string Supplier { get; set; }
+        public string SpecialNeeds { get; set; }
+        
+            
+
+        public int CurrentState { get; set; }
+        public string Creator { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
