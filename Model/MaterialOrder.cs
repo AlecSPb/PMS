e2a1e2f095bd94de9e0aa8d62c9272f18a148466
xs.zhou <newlifechou@outlook.com>
2016-10-10 09:27:43 +0800

    create materialorder and materialorderitem

---
+++ Model/MaterialOrder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 正式原料订单的公共部分
+    /// </summary>
+    public class MaterialOrder
+    {
+        public Guid ID { get; set; }
+        public string OrderLot { get; set; }
+        public string Supplier { get; set; }
+        public string VendorInformation { get; set; }
+        public string ShipToAddress { get; set; }
+        public string ExtraInformation { get; set; }
+
+
+
+
+        public int CurrentState { get; set; }
+        public string Creator { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+
+
+}
---
+++ Model/MaterialOrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 正式原料订单的具体项目
+    /// </summary>
+    public class MaterialOrderItem
+    {
+        public Guid ID { get; set; }
+        public Guid MaterialOrderID { get; set; }
+
+        public string Composition { get; set; }
+        public string Purity { get; set; }
+        public double Weight { get; set; }
+        public string PMIWorkingNumber { get; set; }
+        public string Description { get; set; }
+
+        public double UnitPrice { get; set; }
+    }
+}
