50ddef0c1c3977224a229dca62b70324e4b550de
xs.zhou <newlifechou@outlook.com>
2016-10-11 09:17:54 +0800

    create group access groupaccess

---
+++ Model/PMSGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 用户组
+    /// </summary>
+    public class PMSGroup
+    {
+        public Guid ID { get; set; }
+        public string GroupName { get; set; }
+        public string ExtraInformation { get; set; }
+        public int CurrentState { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ Model/PMSGroupAcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Group 和 Access的多对多关系表
+    /// </summary>
+    public class PMSGroupAcess
+    {
+        public Guid ID { get; set; }
+        public Guid GroupID { get; set; }
+        public Guid AccessID { get; set; }
+    }
+}
