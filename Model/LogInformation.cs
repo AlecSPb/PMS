e00d2c17804bad650a92c4cb8c0fd06f38ac3a5f
xs.zhou <newlifechou@outlook.com>
2016-10-11 15:16:58 +0800

    add log information

---
+++ Model/LogInformation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 谁，什么时间，做了什么事情
+    /// </summary>
+    public class LogInformation
+    {
+        public Guid ID { get; set; }
+        public string CurrentUser { get; set; }
+        public DateTime LogTime { get; set; }
+        public string LogData { get; set; }
+    }
+}
