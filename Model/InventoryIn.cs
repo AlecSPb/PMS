e5f64d2c44f8d1f134efc5f3c80451b474f03a74
xs.zhou <newlifechou@outlook.com>
2016-10-11 16:08:21 +0800

    fill inventoryin and inventoryout

--- Model/InventoryIn.cs
+++ Model/InventoryIn.cs
@@ -8,9 +8,21 @@ namespace Model
 {
     /// <summary>
     /// 库存表In
+    /// 暂时只考虑简单的出入库流水账记录
     /// </summary>
     public class InventoryIn
     {
-        
+        public Guid ID { get; set; }
+        public string ItemName { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+        public string Purity { get; set; }
+
+        public string ComeFrom { get; set; }
+        public string ExtraInformation { get; set; }
+
+        public string Creator { get; set; }
+        public DateTime CreateDate { get; set; }
+
     }
 }
--- Model/InventoryOut.cs
+++ Model/InventoryOut.cs
@@ -8,8 +8,20 @@ namespace Model
 {
     /// <summary>
     /// 库存表Out
+    /// 创建出库表的时候基本信息可以来自入库表
     /// </summary>
     public class InventoryOut
     {
+        public Guid ID { get; set; }
+        public string ItemName { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+        public string Purity { get; set; }
+
+        public string GoTo { get; set; }
+        public string ExtraInformation { get; set; }
+
+        public string Creator { get; set; }
+        public DateTime CreateDate { get; set; }
     }
 }
