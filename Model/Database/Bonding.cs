c963eb3ba55a07744a6a61c026b525cb8e31a9ac
xs.zhou <newlifechou@outlook.com>
2016-11-26 18:16:00 +0800

    add bonding class to database model

---
+++ Model/Database/Bonding.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Database
+{
+    public class Bonding
+    {
+        public Guid ID { get; set; }
+        public Guid OrderID { get; set; }
+        public string DimensionCheck { get; set; }
+        public string WarpCheck { get; set; }
+        public string BackplateCheck { get; set; }
+        public string TargetPreProcessCheck { get; set; }
+        public string BackPlatePreProcessCheck { get; set; }
+        public string BondingRecord { get; set; }
+        public string AfterCheck { get; set; }
+        public string Remark { get; set; }
+    }
+}
