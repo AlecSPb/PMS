acd1157abdd9e8f45dff959ea5e47b20cec541ec
xs.zhou <newlifechou@outlook.com>
2016-12-08 09:05:15 +0800

    edit loginview ui

--- PMSTabletClient/MainWindow.xaml.cs
+++ PMSTabletClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PMSTabletClient.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +24,7 @@ namespace PMSTabletClient
         public MainWindow()
         {
             InitializeComponent();
-            contentPart.Content = new ProductView();
+            contentPart.Content = new LogInView();
         }
     }
 }
