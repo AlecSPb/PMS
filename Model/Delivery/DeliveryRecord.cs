ee4e38d662974fe846467a08df5e8a5fe306be8c
xs.zhou <newlifechou@outlook.com>
2016-12-11 16:07:38 +0800

    add delivery model to pms solution

---
+++ Model/Delivery/DeliveryRecord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Delivery
+{
+    //发货单
+    public class DeliveryRecord
+    {
+        public Guid ID { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string Creator { get; set; }
+        public int State { get; set; }
+
+        public string PONumber { get; set; }
+
+        public string Delivery { get; set; }
+        public string DeliveryNumber { get; set; }
+
+        public string  Desitination { get; set; }
+        public DateTime DeliveryTime { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public string Remark { get; set; }
+
+        public string PackageWeightStatus { get; set; }
+
+
+    }
+}
---
+++ Model/Delivery/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Delivery
+{
+    public class Product
+    {
+        public Guid ID { get; set; }
+        public string ProductID { get; set; }
+        public string Composition { get; set; }
+        public string PO { get; set; }
+        public string Dimension { get; set; }
+        public string Remark { get; set; }
+    }
+}
