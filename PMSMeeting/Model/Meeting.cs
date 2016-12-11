786547438271c52c53d4f7c5d37874f2b91f7b8c
xs.zhou <newlifechou@outlook.com>
2016-12-11 15:46:40 +0800

    create meeting class

---
+++ PMSMeeting/Model/Meeting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSMeeting.Model
+{
+    public class Meeting
+    {
+        public Guid ID { get; set; }
+        public string Creator { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime MeetingTime { get; set; }
+        public string Holder { get; set; }
+        public string Title { get; set; }
+        public string MainTheme { get; set; }
+        public string Persons { get; set; }
+        public string Records{ get; set; }
+        public bool State { get; set; }//On Cancel
+
+
+    }
+}
