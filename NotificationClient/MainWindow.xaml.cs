d98c5f46cb2bf08df0e38ef18609c3140482ae1e
xs.zhou <newlifechou@outlook.com>
2016-12-19 15:56:00 +0800

    binding by code successfully

--- NotificationClient/MainWindow.xaml.cs
+++ NotificationClient/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -18,12 +20,49 @@ namespace NotificationClient
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window,INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
-            
+        }
+        #region Properties
+        private double windowWidth;
+        public double WindowWidth
+        {
+            get { return windowWidth; }
+            set { windowWidth = value; RaisePropertyChanged(nameof(WindowWidth)); }
+        }
+
+        private double textblockWidth;
+        public double TextblockWidth
+        {
+            get { return textblockWidth; }
+            set { textblockWidth = value; RaisePropertyChanged(nameof(TextblockWidth)); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        private void window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetAndStart();
+        }
+
+        private void SetAndStart()
+        {
+            WindowWidth = window.ActualWidth;
+            TextblockWidth = txtInformation.ActualWidth;
+            var storyboard = (Storyboard)this.FindResource("InformationScrollAnimation");
+            storyboard.Begin();
         }
     }
 }
