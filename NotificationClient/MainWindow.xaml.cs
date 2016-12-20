2a8924e8800fc5413464a99fb1a3c3fd768ce169
xs.zhou <newlifechou@outlook.com>
2016-12-20 11:32:37 +0800

    stop here

--- NotificationClient/MainWindow.xaml.cs
+++ NotificationClient/MainWindow.xaml.cs
@@ -20,59 +20,14 @@ namespace NotificationClient
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-        #region Properties
-        private double startPosition;
-        public double StartPosition
-        {
-            get { return startPosition; }
-            set { startPosition = value; RaisePropertyChanged(nameof(StartPosition)); }
-        }
 
-        private double endPosition;
-        public double EndPosition
-        {
-            get { return endPosition; }
-            set { endPosition = value; RaisePropertyChanged(nameof(EndPosition)); }
-        }
+       //string msg = "会议通知：2016-12-19 13:30 在新会议室举行上一批产品问题解决方案相关的会议 参加人员:习近平 胡锦涛 江泽民 李鹏 邓小平";
 
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void RaisePropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-        #endregion
-
-        private void window_Loaded(object sender, RoutedEventArgs e)
-        {
-            string msg = "会议通知：2016-12-19 13:30 在新会议室举行上一批产品问题解决方案相关的会议 参加人员:习近平 胡锦涛 江泽民 李鹏 邓小平";
-            SetAndStart(msg);
-        }
-
-        private void SetAndStart(string content)
-        {
-            txtInformation.Text = content;
-
-            StartPosition = window.ActualWidth;
-            EndPosition = txtInformation.ActualWidth*-1;
-
-            var storyboard = (Storyboard)this.FindResource("InformationScrollAnimation");
-            storyboard.Begin();
-        }
-
-        private void window_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            StartPosition = window.ActualWidth;
-            EndPosition = txtInformation.ActualWidth * -1;
-        }
     }
 }
--- NotificationClient/NegativeConverter.cs
+++ NotificationClient/NegativeConverter.cs
@@ -4,22 +4,28 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Data;
 
 namespace NotificationClient
 {
-    public class MarginConverterTo : IValueConverter
+    public class NegativeConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            double leftMargin = -(double)value;
-            return new Thickness(leftMargin, 0, 0, 0);
+            if (value is double)
+            {
+                return -((double)value);
+            }
+            return value;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is double)
+            {
+                return +(double)value;
+            }
+            return value;
         }
     }
 }
