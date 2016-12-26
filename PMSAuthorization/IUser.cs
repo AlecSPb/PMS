d5b789978329d35523cd9c9017f3478ace53fe09
xs.zhou <newlifechou@outlook.com>
2016-12-26 09:38:16 +0800

    add IUser interface

---
+++ PMSAuthorization/IUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSAuthorization
+{
+    public interface IUser
+    {
+        bool IsExsit(User user);
+        Role GetRole(User user);
+
+        User GetUserByUserName(string userName);
+        IList<User> GetAllUsers();
+        int AddUser(User user);
+        int UpdateUser(User user);
+        int DisableUser(User user);
+    }
+}
