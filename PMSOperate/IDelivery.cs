68cf35684718049dbc90b9296d955f20fc106cd6
xs.zhou <newlifechou@outlook.com>
2017-01-09 15:16:33 +0800

    change ProjectIService to ProjectIRepository

--- PMSOperate/IDelivery.cs
+++ PMSOperate/IDelivery.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PMSIService
+namespace PMSIRepository
 {
     public interface IDelivery
     {
--- PMSOperate/IProduct.cs
+++ PMSOperate/IProduct.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using PMSModel;
 
-namespace PMSIService
+namespace PMSIRepository
 {
     public interface IProduct
     {
