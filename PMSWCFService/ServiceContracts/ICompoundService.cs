a7e6ef9ee9722a684bf535884d1ee2ee5e8605c0
xs.zhou <newlifechou@outlook.com>
2017-01-15 23:18:56 +0800

    add more interface

---
+++ PMSWCFService/ServiceContracts/ICompoundService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using PMSWCFService.DataContracts;
+
+namespace PMSWCFService.ServiceContracts
+{
+    public interface ICompoundService
+    {
+        List<DcCompound> GetAllCompounds();
+        int AddCompound(DcCompound model);
+        int UpdateCompound(DcCompound model);
+        int DeleteCompound(DcCompound model);
+    }
+}
---
+++ PMSWCFService/ServiceContracts/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMSWCFService.ServiceContracts
+{
+    public interface ICustomerService
+    {
+    }
+}
