bff7c4ace97d475875162a580284f6c97c4b3f65
xs.zhou <newlifechou@outlook.com>
2017-01-18 11:37:42 +0800

    finsih recordproduct service

--- PMSWCFService/PMSRecordProductService.cs
+++ PMSWCFService/PMSRecordProductService.cs
@@ -6,6 +6,7 @@ using PMSWCFService.DataContracts;
 using PMSWCFService.ServiceContracts;
 using PMSDAL;
 using AutoMapper;
+using PMSCommon;
 
 namespace PMSWCFService
 {
@@ -24,24 +25,54 @@ namespace PMSWCFService
             }
         }
 
-        public int DeleteRecordProduct(DcRecordProduct model)
+        public int DeleteRecordProduct(Guid id)
         {
-            throw new NotImplementedException();
+            using (var dc = new PMSDbContext())
+            {
+                int result = 0;
+                var product = dc.Products.Find(id);
+                if (product != null)
+                {
+                    dc.Products.Remove(product);
+                    result = dc.SaveChanges();
+                }
+
+                return result;
+            }
         }
 
         public List<DcRecordProduct> GetRecordProductBySearchInPage(int skip, int take, string productId, string compositionStd)
         {
-            throw new NotImplementedException();
+            using (var dc = new PMSDbContext())
+            {
+                var result = dc.Products.Where(p => p.ProductID.Contains(productId) && p.Composition.Contains(compositionStd)
+                  && p.State != (int)ModelState.Deleted).OrderByDescending(p => p.CreateTime).Skip(skip).Take(take).ToList();
+                Mapper.Initialize(cfg => cfg.CreateMap<RecordProduct, DcRecordProduct>());
+                var products = Mapper.Map<List<RecordProduct>, List<DcRecordProduct>>(result);
+                return products;
+            }
         }
 
-        public int GetRecordProductCountBySearchInPage(int skip, int take, string productid, string compositionStd)
+        public int GetRecordProductCountBySearchInPage(string productId, string compositionStd)
         {
-            throw new NotImplementedException();
+            using (var dc = new PMSDbContext())
+            {
+                return dc.Products.Where(p => p.ProductID.Contains(productId) && p.Composition.Contains(compositionStd)
+                  && p.State != (int)ModelState.Deleted).Count();
+            }
         }
 
         public int UpdateRecordProduct(DcRecordProduct model)
         {
-            throw new NotImplementedException();
+            using (var dc = new PMSDbContext())
+            {
+                int result = 0;
+                Mapper.Initialize(cfg => cfg.CreateMap<DcRecordProduct, RecordProduct>());
+                var product = Mapper.Map<RecordProduct>(model);
+                dc.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                result = dc.SaveChanges();
+                return result;
+            }
         }
     }
 }
--- PMSWCFService/ServiceContracts/IRecordProductService.cs
+++ PMSWCFService/ServiceContracts/IRecordProductService.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using PMSWCFService.DataContracts;
+using System.ServiceModel;
 
 namespace PMSWCFService.ServiceContracts
 {
+    [ServiceContract]
     public interface IRecordProductService
     {
+        [OperationContract]
         List<DcRecordProduct> GetRecordProductBySearchInPage(int skip, int take, string productId, string compositionStd);
-        int GetRecordProductCountBySearchInPage(int skip, int take, string productid, string compositionStd);
+        [OperationContract]
+        int GetRecordProductCountBySearchInPage(string productId, string compositionStd);
+        [OperationContract]
         int AddRecordProduct(DcRecordProduct model);
+        [OperationContract]
         int UpdateRecordProduct(DcRecordProduct model);
-        int DeleteRecordProduct(DcRecordProduct model);
+        [OperationContract]
+        int DeleteRecordProduct(Guid id);
     }
 }
