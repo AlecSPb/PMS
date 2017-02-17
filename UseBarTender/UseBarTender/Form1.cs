f2b47325ee3a3d9fa3428a0158504e1a1f84d382
xs.zhou <newlifechou@outlook.com>
2017-02-17 22:26:10 +0800

    test bartender print,ok

---
+++ UseBarTender/UseBarTender/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using bt=BarTender;
+
+namespace UseBarTender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bt.Application btApp;
+        bt.Format btnFormat;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string msg = "hello world";
+            btApp = new bt.Application();
+            btnFormat = btApp.Formats.Open(@"C:\Users\newli\Documents\BarTender\BarTender Documents\10070.btw", false, "");
+            btnFormat.PrintSetup.IdenticalCopiesOfLabel = 1;
+            btnFormat.PrintSetup.NumberSerializedLabels = 1;
+            btnFormat.SetNamedSubStringValue("Content", msg);
+
+            btnFormat.PrintOut(true, true);
+            btnFormat.Close(bt.BtSaveOptions.btSaveChanges);
+
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            btApp.Quit(bt.BtSaveOptions.btSaveChanges);
+            base.OnClosing(e);
+        }
+    }
+}
