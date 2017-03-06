5e14fd787cf92e987ee8c1d01cb14ccb066d182d
xs.zhou <newlifechou@outlook.com>
2017-03-06 20:48:16 +0800

    add new entities in dal

--- PMSDAL/Entities/BDSieve.cs
+++ PMSDAL/Entities/BDSieve.cs
@@ -4,26 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PMSDAL
+namespace PMSDAL.Entities
 {
-    public class BDVHPMold
+    public class BDSieve
     {
         public Guid ID { get; set; }
-        public string MoldName { get; set; }
-        public string MoldType { get; set; }
-        public double InnerDiameter { get; set; }
-        public double ModelHeight { get; set; }
-        public string MoldDetails { get; set; }
-
+        public string SieveName { get; set; }
+        public string Mesh { get; set; }
         public DateTime StartTime { get; set; }
+        public string Manufacuture { get; set; }
         public int EstimateUsedCount { get; set; }
-        public int CurrentUsedCount { get; set; }
-
+        public int CurrentCount { get; set; }
 
         public string State { get; set; }
         public string Creator { get; set; }
         public DateTime CreateTime { get; set; }
-
     }
-
 }
