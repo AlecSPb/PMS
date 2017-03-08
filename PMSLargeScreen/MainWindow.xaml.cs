7714cd2b7a65585c43b1c03127c6501b450d6309
xs.zhou <newlifechou@outlook.com>
2017-03-08 08:05:58 +0800

    use static models in largescreen

--- PMSLargeScreen/MainWindow.xaml.cs
+++ PMSLargeScreen/MainWindow.xaml.cs
@@ -78,54 +78,58 @@ namespace PMSLargeScreen
         private void LoadDisplayData()
         {
             #region TestData
-            //var firstModel = new SinglePanelModel();
-            //firstModel.DeviceCode = "A";
-            //firstModel.MoldType = "CFC";
-            //firstModel.MoldDiameter = 233;
-            //firstModel.Temperature = 500;
-            //firstModel.Pressure = 345;
-            //firstModel.Vaccum = 1E-3;
-            //firstModel.ProcessCode = "W1";
-            //firstModel.Compositions = new List<string>()
-            //{
-            //    "CuGaSe2"+"   共2片"+" W1",
-            //    "Cu22.8In20Ga7.0Se50.2"+"   共1片"+" W3"
-            //};
+            var firstModel = new SinglePanelModel();
+            firstModel.DeviceCode = "A";
+            firstModel.MoldType = "CFC";
+            firstModel.MoldDiameter = 233;
+            firstModel.Temperature = 500;
+            firstModel.Pressure = 345;
+            firstModel.Vaccum = 1E-3;
+            firstModel.ProcessCode = "W1";
+            firstModel.Compositions = new List<string>()
+            {
+                "CuGaSe2"+"   共2片"+" W1",
+                "Cu22.8In20Ga7.0Se50.2"+"   共1片"+" W3"
+            };
 
-            //var secondModel = new SinglePanelModel();
-            //secondModel.DeviceCode = "B";
-            //secondModel.MoldType = "CFC";
-            //secondModel.MoldDiameter = 233;
-            //secondModel.Temperature = 500;
-            //secondModel.Pressure = 345;
-            //secondModel.Vaccum = 1E-3;
-            //secondModel.ProcessCode = "W1";
-            //secondModel.Compositions = new List<string>()
-            //{
-            //    "Cu22.8In20Ga7.0Se50.2"+" 共1片"+" W2",
-            //    "CuGaSe2"+" 共2片"+" W2"
-            //};
+            var secondModel = new SinglePanelModel();
+            secondModel.DeviceCode = "B";
+            secondModel.MoldType = "CFC";
+            secondModel.MoldDiameter = 233;
+            secondModel.Temperature = 500;
+            secondModel.Pressure = 345;
+            secondModel.Vaccum = 1E-3;
+            secondModel.ProcessCode = "W1";
+            secondModel.Compositions = new List<string>()
+            {
+                "Cu22.8In20Ga7.0Se50.2"+" 共1片"+" W2",
+                "CuGaSe2"+" 共2片"+" W2"
+            };
 
-            //var thirdModel = new SinglePanelModel();
-            //thirdModel.DeviceCode = "C";
-            //thirdModel.MoldType = "CFC";
-            //thirdModel.MoldDiameter = 233;
-            //thirdModel.Temperature = 500;
-            //thirdModel.Pressure = 345;
-            //thirdModel.Vaccum = 1E-3;
-            //thirdModel.ProcessCode = "W3";
-            //thirdModel.Compositions = new List<string>()
-            //{
-            //    "Cu22.8In20Ga7.0Se50.2"+"共1片"+"W3",
-            //    "CuGaSe2"+"共2片"+" W3"
-            //};
+            var thirdModel = new SinglePanelModel();
+            thirdModel.DeviceCode = "C";
+            thirdModel.MoldType = "CFC";
+            thirdModel.MoldDiameter = 233;
+            thirdModel.Temperature = 500;
+            thirdModel.Pressure = 345;
+            thirdModel.Vaccum = 1E-3;
+            thirdModel.ProcessCode = "W3";
+            thirdModel.Compositions = new List<string>()
+            {
+                "Cu22.8In20Ga7.0Se50.2"+"共1片"+"W3",
+                "CuGaSe2"+"共2片"+" W3"
+            };
             #endregion
 
-            var todayList = LargeScreenService.GetTodayMissonWithPlan();
+            //var todayList = LargeScreenService.GetTodayMissonWithPlan();
 
-            AddIntoModel(todayList, "A");
-            AddIntoModel(todayList, "B");
-            AddIntoModel(todayList, "C");
+            //AddIntoModel(todayList, "A");
+            //AddIntoModel(todayList, "B");
+            //AddIntoModel(todayList, "C");
+            Models.Add(firstModel);
+            Models.Add(secondModel);
+            Models.Add(thirdModel);
+
 
             if (Models.Count > 0)
             {
