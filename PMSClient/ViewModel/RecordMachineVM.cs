df45f7af95786c3979afd07025a29602366b3734
xs.zhou <newlifechou@outlook.com>
2017-03-23 15:03:23 +0800

    finish recordmachine vm

--- PMSClient/ViewModel/RecordMachineVM.cs
+++ PMSClient/ViewModel/RecordMachineVM.cs
@@ -14,7 +14,49 @@ namespace PMSClient.ViewModel
     {
         public RecordMachineVM()
         {
+
             PageChanged = new RelayCommand(ActionPaging);
+            Add = new RelayCommand(ActionAdd);
+            Edit = new RelayCommand<DcRecordDeMold>(ActionEdit);
+            Search = new RelayCommand(ActionSearch);
+            All = new RelayCommand(ActionAll);
+            SetPageParametersWhenConditionChange();
+        }
+
+        private void ActionAll()
+        {
+            SetPageParametersWhenConditionChange();
+        }
+
+        private void ActionSearch()
+        {
+            SetPageParametersWhenConditionChange();
+        }
+
+        private void ActionEdit(DcRecordDeMold model)
+        {
+            NavigationService.GoTo(VToken.RecordMachineEdit, new ModelObject() { IsNew = false, Model = model });
+        }
+
+        private void ActionAdd()
+        {
+            var model = new DcRecordMachine();
+            model.ID = Guid.NewGuid();
+            model.CreateTime = DateTime.Now;
+            model.Creator = CurrentUserInformation.UserName;
+            model.State = PMSCommon.CommonState.Checked.ToString();
+            model.VHPPlanLot = DateTime.Now.ToString("yyMMdd");
+            model.Composition = "成分";
+            model.Diameter1 = 0;
+            model.Diameter2 = 0;
+
+            model.Thickness1 = 0;
+            model.Thickness2 = 0;
+            model.Thickness3 = 0;
+            model.Thickness4 = 0;
+            model.ExtraRequirement = "无缺口无划痕";
+
+            NavigationService.GoTo(VToken.RecordMachineEdit, new ModelObject() { IsNew = true, Model = model });
         }
 
         private void SetPageParametersWhenConditionChange()
@@ -39,5 +81,8 @@ namespace PMSClient.ViewModel
 
 
         public ObservableCollection<DcRecordMachine> RecordMachines { get; set; }
+
+        public RelayCommand Add { get; set; }
+        public RelayCommand<DcRecordDeMold> Edit { get; set; }
     }
 }
