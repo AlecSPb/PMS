1475021b9669f8b8aebdecdff073a44aea492f6f
xs.zhou <newlifechou@outlook.com>
2017-03-26 15:01:59 +0800

    fix config of element service

--- PMSWCFService/ServiceContracts/IElementService.cs
+++ PMSWCFService/ServiceContracts/IElementService.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using PMSWCFService.DataContracts;
 
-namespace PMSWCFService
+namespace PMSWCFService.ServiceContracts
 {
     [ServiceContract]
     public interface IElementService
