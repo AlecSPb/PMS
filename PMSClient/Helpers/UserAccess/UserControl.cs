c192fd8330e726b4954eb8cea61f1dc14d1a4c89
xs.zhou <newlifechou@outlook.com>
2017-03-26 10:16:05 +0800

    Add UserControl class

---
+++ PMSClient/Helpers/UserAccess/UserControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMSClient.UserService;
+
+namespace PMSClient.Helpers
+{
+    /// <summary>
+    /// 存储和处理客户端所需要的用户信息
+    /// </summary>
+    public class UserControl
+    {
+        public DcUser CurrentUser { get; set; }
+        public DcUserRole CurrentUserRole { get; set; }
+        public List<DcUserAccess> CurrentAccesses { get; set; }
+
+
+    }
+}
