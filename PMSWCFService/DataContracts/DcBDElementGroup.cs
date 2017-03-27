0a4b25987f15ea6091642a831abfbc1b196914c5
xs.zhou <newlifechou@outlook.com>
2017-03-27 12:26:40 +0800

    add dcelementgroup data contracts

---
+++ PMSWCFService/DataContracts/DcBDElementGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace PMSWCFService.DataContracts
+{
+    [DataContract]
+    public class BDElementGroup
+    {
+        [DataMember]
+        public Guid ID { get; set; }
+        [DataMember]
+        public string GroupName { get; set; }
+        [DataMember]
+        public string CreateTime { get; set; }
+    }
+}
---
+++ PMSWCFService/DataContracts/DcBDElementGroupItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace PMSWCFService.DataContracts
+{
+    [DataContract]
+    public class BDElementGroupItem
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+        [DataMember]
+        public Guid GroupElementID { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int GroupNumber { get; set; }
+        [DataMember]
+        public double MolWeight { get; set; }
+        [DataMember]
+        public double At { get; set; }
+    }
+}
