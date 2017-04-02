c136b3634b1e757877e8914981a01562e7814c8b
xs.zhou <newlifechou@outlook.com>
2017-04-02 09:11:43 +0800

    add logservice into wcfservice project

---
+++ PMSWCFService/Log/LogService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMSDAL;
+
+namespace PMSWCFService.Log
+{
+    public class Log
+    {
+        
+        public Log()
+        {
+
+        }
+
+
+        public void Information(string information)
+        {
+            try
+            {
+                using (var dc = new PMSDbContext())
+                {
+                    var entity = new LogInformation()
+                    {
+                        ID = Guid.NewGuid(),
+                        CreateTime = DateTime.Now,
+                        Log = information
+                    };
+                    dc.LogInformations.Add(entity);
+                    dc.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        public void Error(Exception ex)
+        {
+            try
+            {
+                if (ex!=null)
+                {
+                    using (var dc=new PMSDbContext())
+                    {
+                        var entity = new LogError()
+                        {
+                            ID = Guid.NewGuid(),
+                            CreateTime = DateTime.Now,
+                            Error = ex.Message
+                        };
+
+                        dc.LogErrors.Add(entity);
+                        dc.SaveChanges();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
