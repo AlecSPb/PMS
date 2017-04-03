dc41c9b0f2ef9a051ba9ff40feb63b73ea1f62fd
xs.zhou <newlifechou@outlook.com>
2017-04-03 09:47:58 +0800

    add exception process into recordtestservice

--- PMSWCFService/ServiceImplements/RecordTestService.cs
+++ PMSWCFService/ServiceImplements/RecordTestService.cs
@@ -14,65 +14,110 @@ namespace PMSWCFService
     {
         public int AddRecordTest(DcRecordTest model)
         {
-            using (var dc = new PMSDbContext())
+            try
             {
-                int result = 0;
-                Mapper.Initialize(cfg => cfg.CreateMap<DcRecordTest, RecordTest>());
-                var product = Mapper.Map<RecordTest>(model);
-                dc.RecordTests.Add(product);
-                result = dc.SaveChanges();
-                return result;
+                using (var dc = new PMSDbContext())
+                {
+                    int result = 0;
+                    Mapper.Initialize(cfg => cfg.CreateMap<DcRecordTest, RecordTest>());
+                    var product = Mapper.Map<RecordTest>(model);
+                    dc.RecordTests.Add(product);
+                    result = dc.SaveChanges();
+                    return result;
+                }
             }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw ex;
+            }
+
         }
 
         public int DeleteRecordTest(Guid id)
         {
-            using (var dc = new PMSDbContext())
+            try
             {
-                int result = 0;
-                var product = dc.RecordTests.Find(id);
-                if (product != null)
+                using (var dc = new PMSDbContext())
                 {
-                    dc.RecordTests.Remove(product);
-                    result = dc.SaveChanges();
-                }
+                    int result = 0;
+                    var product = dc.RecordTests.Find(id);
+                    if (product != null)
+                    {
+                        dc.RecordTests.Remove(product);
+                        result = dc.SaveChanges();
+                    }
 
-                return result;
+                    return result;
+                }
             }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw ex;
+            }
+
         }
 
         public List<DcRecordTest> GetRecordTestBySearchInPage(int skip, int take, string productId, string compositionStd)
         {
-            using (var dc = new PMSDbContext())
+            try
             {
-                var result = dc.RecordTests.Where(p => p.ProductID.Contains(productId) && p.Composition.Contains(compositionStd)
-                  && p.State != OrderState.Deleted.ToString()).OrderByDescending(p => p.CreateTime).Skip(skip).Take(take).ToList();
-                Mapper.Initialize(cfg => cfg.CreateMap<RecordTest, DcRecordTest>());
-                var products = Mapper.Map<List<RecordTest>, List<DcRecordTest>>(result);
-                return products;
+                using (var dc = new PMSDbContext())
+                {
+                    var result = dc.RecordTests.Where(p => p.ProductID.Contains(productId) && p.Composition.Contains(compositionStd)
+                      && p.State != OrderState.Deleted.ToString()).OrderByDescending(p => p.CreateTime).Skip(skip).Take(take).ToList();
+                    Mapper.Initialize(cfg => cfg.CreateMap<RecordTest, DcRecordTest>());
+                    var products = Mapper.Map<List<RecordTest>, List<DcRecordTest>>(result);
+                    return products;
+                }
             }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw ex;
+            }
+
         }
 
         public int GetRecordTestCountBySearchInPage(string productId, string compositionStd)
         {
-            using (var dc = new PMSDbContext())
+            try
             {
-                return dc.RecordTests.Where(p => p.ProductID.Contains(productId) && p.Composition.Contains(compositionStd)
-                  && p.State != OrderState.Deleted.ToString()).Count();
+                using (var dc = new PMSDbContext())
+                {
+                    return dc.RecordTests.Where(p => p.ProductID.Contains(productId) && p.Composition.Contains(compositionStd)
+                      && p.State != OrderState.Deleted.ToString()).Count();
+                }
             }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw ex;
+            }
+
         }
 
         public int UpdateRecordTest(DcRecordTest model)
         {
-            using (var dc = new PMSDbContext())
+            try
             {
-                int result = 0;
-                Mapper.Initialize(cfg => cfg.CreateMap<DcRecordTest, RecordTest>());
-                var product = Mapper.Map<RecordTest>(model);
-                dc.Entry(product).State = System.Data.Entity.EntityState.Modified;
-                result = dc.SaveChanges();
-                return result;
+                using (var dc = new PMSDbContext())
+                {
+                    int result = 0;
+                    Mapper.Initialize(cfg => cfg.CreateMap<DcRecordTest, RecordTest>());
+                    var product = Mapper.Map<RecordTest>(model);
+                    dc.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                    result = dc.SaveChanges();
+                    return result;
+                }
             }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw ex;
+            }
+
         }
     }
 }
