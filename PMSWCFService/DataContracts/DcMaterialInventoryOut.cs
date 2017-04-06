035a3ae300f84afd92fd98d1e2f315a1a81e16fc
xs.zhou <newlifechou@outlook.com>
2017-04-06 17:16:30 +0800

    add materialinventory data contract in wcf service

---
+++ PMSWCFService/DataContracts/DcMaterialInventoryOut.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSWCFService.DataContracts
+{
+    [DataContract]
+    public class PMSMaterialInventoryOut
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+        [DataMember]
+        public string State { get; set; }
+        [DataMember]
+        public string Creator { get; set; }
+        [DataMember]
+        public DateTime CreateTime { get; set; }
+        [DataMember]
+        public string Composition { get; set; }
+        [DataMember]
+        public string Purity { get; set; }
+        [DataMember]
+        public string Supplier { get; set; }
+        [DataMember]
+        public double Weight { get; set; }
+        [DataMember]
+        public string Remark { get; set; }
+    }
+}
