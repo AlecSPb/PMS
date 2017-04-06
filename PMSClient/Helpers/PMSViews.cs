c8d883a0383a56a80bd747dd782a2db7a844b48b
xs.zhou <newlifechou@outlook.com>
2017-04-06 10:48:28 +0800

    add vm as datacontext to ordereditview and create new pmsviews tokens collection

---
+++ PMSClient/Helpers/PMSViews.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSClient.Helpers
+{
+    public enum PMSViews
+    {
+        Navigation,
+        Order,
+        OrderEdit
+    }
+}
