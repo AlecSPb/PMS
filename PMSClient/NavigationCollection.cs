79cb4aa763b5d632a1a90e5414672f7fa4c72f2a
xs.zhou <newlifechou@outlook.com>
2017-04-06 10:41:14 +0800

    add navigationcollection static class

---
+++ PMSClient/NavigationCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMSClient.ViewModel;
+using PMSClient.ViewForDesktop;
+using PMSClient.ViewForTablet;
+
+
+namespace PMSClient
+{
+    /// <summary>
+    /// 提供本项目各类视图和视图模型类的引用
+    /// </summary>
+    public static class NavigationCollection
+    {
+        private static DesktopViewLocator _desktopViews;
+        private static TabletViewLocator _tabletViews;
+
+        private static ViewModelLocator _viewModels;
+        static NavigationCollection()
+        {
+            _desktopViews = new DesktopViewLocator();
+            _tabletViews = new TabletViewLocator();
+            _viewModels = (App.Current as App).FindResource("Locator") as ViewModelLocator;
+
+        }
+
+        public static DesktopViewLocator DesktopViews
+        {
+            get { return _desktopViews; }
+        }
+        public static TabletViewLocator TabletViews
+        {
+            get { return _tabletViews; }
+        }
+
+        public static ViewModelLocator ViewModels
+        {
+            get { return _viewModels; }
+        }
+    }
+}
