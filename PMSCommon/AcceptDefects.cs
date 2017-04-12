9fca638c2bf554772d35df081db2e9c57cd35011
xs.zhou <newlifechou@outlook.com>
2017-04-12 16:26:19 +0800

    add more enums

---
+++ PMSCommon/AcceptDefects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSCommon
+{
+    /// <summary>
+    /// 可接受缺陷
+    /// </summary>
+    public enum AcceptDefects
+    {
+        正常要求,
+        特殊要求
+    }
+}
---
+++ PMSCommon/OrderUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSCommon
+{
+    /// <summary>
+    /// 订单单位
+    /// </summary>
+    public enum OrderUnit
+    {
+        片,
+        套,
+        Kg,
+        g
+    }
+}
