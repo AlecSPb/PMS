2695332e54cdd93d6a0fdbde7d10cfc4389e95d0
xs.zhou <newlifechou@outlook.com>
2017-04-16 00:05:37 +0800

    caculate ratio of  powder loss

--- PMSClient/ViewForDesktop/RecordMillingEditView.xaml.cs
+++ PMSClient/ViewForDesktop/RecordMillingEditView.xaml.cs
@@ -24,5 +24,27 @@ namespace PMSClient.ViewForDesktop
         {
             InitializeComponent();
         }
+
+        private void btnCalculation_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double weightIn = double.Parse(txtWeightIn.Text.Trim());
+                double weightOut = double.Parse(txtWeightOut.Text.Trim());
+                double weightRemain = double.Parse(txtWeightRemain.Text.Trim());
+                double loss = weightIn - weightOut - weightRemain;
+                if (weightIn > 0 && loss >= 0)
+                {
+                    double ratio = loss / weightIn;
+                    txtRatio.Text = ratio.ToString();
+                    txtRatio.Focus();
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
     }
 }
