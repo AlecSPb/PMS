4839c4df0fbca2ccf54516e6a5f7525b11d6f341
xs.zhou <newlifechou@outlook.com>
2017-04-16 11:24:59 +0800

    add tip to inventory edit view

--- PMSClient/ViewModel/MaterialOrderItemSelectVM.cs
+++ PMSClient/ViewModel/MaterialOrderItemSelectVM.cs
@@ -58,7 +58,7 @@ namespace PMSClient.ViewModel
         private void SetPageParametersWhenConditionChange()
         {
             PageIndex = 1;
-            PageSize = 10;
+            PageSize = 20;
             var service = new MaterialOrderServiceClient();
             RecordCount = service.GetMaterialOrderItemsCount();
             service.Close();
