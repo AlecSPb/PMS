4f5b3dd3ae748c0b4531f07d604f34413da8ac4a
xs.zhou <newlifechou@outlook.com>
2017-04-17 22:49:58 +0800

    finish recordbonding

---
+++ AuthorizationChecker/Checker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorizationChecker
+{
+    public static  class Checker
+    {
+
+        public static void CheckIfCanRun()
+        {
+            //最后使用期限
+            var untilTime = new DateTime(2018, 1, 1);
+            if (DateTime.Now>untilTime)
+            {
+                throw new Exception("服务器错误，代码610324,请联系管理员");
+            }
+        }
+    }
+}
