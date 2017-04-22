94beadef3b7f3db12cc6e1d7606ec4a041fbb1da
xs.zhou <newlifechou@outlook.com>
2017-04-22 10:05:55 +0800

    add vhpplantype to pmscommon

---
+++ PMSCommon/VHPPlanType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSCommon
+{
+    /// <summary>
+    /// 热压计划类型
+    /// 指示热压目的
+    /// </summary>
+    public enum VHPPlanType
+    {
+        加工,
+        回收,
+        烤料,
+        其他
+    }
+}
