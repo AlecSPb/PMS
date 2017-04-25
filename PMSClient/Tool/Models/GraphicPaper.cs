cff5b493040e85716f6bd2f0c3f7cc3d534ca4cc
xs.zhou <newlifechou@outlook.com>
2017-04-25 15:06:24 +0800

    start to make density

--- PMSClient/Tool/Models/GraphicPaper.cs
+++ PMSClient/Tool/Models/GraphicPaper.cs
@@ -22,16 +22,19 @@ namespace PMSClient.Tool.Models
            get
             {
                 var papers = new List<GraphicPaper>();
-                papers.Add(new GraphicPaper { Diameter=80,Weight=4});
-                papers.Add(new GraphicPaper { Diameter = 80, Weight = 4 });
-                papers.Add(new GraphicPaper { Diameter = 105, Weight = 4 });
-                papers.Add(new GraphicPaper { Diameter = 128, Weight = 4 });
-                papers.Add(new GraphicPaper { Diameter = 205, Weight = 4 });
-                papers.Add(new GraphicPaper { Diameter = 206, Weight = 4 });
-                papers.Add(new GraphicPaper { Diameter = 233, Weight = 4 });
-                papers.Add(new GraphicPaper { Diameter = 255, Weight = 4 });
-                papers.Add(new GraphicPaper { Diameter = 306, Weight = 4 });
-                papers.Add(new GraphicPaper { Diameter = 455, Weight = 4 });
+                papers.Add(new GraphicPaper { Diameter=0,Weight=0});
+                papers.Add(new GraphicPaper { Diameter = 80, Weight = 4.4 });
+                papers.Add(new GraphicPaper { Diameter = 105, Weight = 6.4 });
+                papers.Add(new GraphicPaper { Diameter = 128, Weight = 11 });
+                papers.Add(new GraphicPaper { Diameter = 155, Weight = 14.8 });
+                papers.Add(new GraphicPaper { Diameter = 160, Weight = 19 });
+                papers.Add(new GraphicPaper { Diameter = 206, Weight = 24.9 });
+                papers.Add(new GraphicPaper { Diameter = 233, Weight =33 });
+                papers.Add(new GraphicPaper { Diameter = 255, Weight = 43 });
+                papers.Add(new GraphicPaper { Diameter = 306, Weight = 65.4 });
+                papers.Add(new GraphicPaper { Diameter = 319, Weight = 70.2 });
+                papers.Add(new GraphicPaper { Diameter = 336, Weight = 70.2 });
+                papers.Add(new GraphicPaper { Diameter = 450, Weight = 77 });
                 return papers;
             }
         }
