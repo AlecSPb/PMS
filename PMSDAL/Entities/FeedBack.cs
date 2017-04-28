9d93c51ec8f9b3b6c7f889a76619f419750fbdf5
xs.zhou <newlifechou@outlook.com>
2017-04-28 19:19:36 +0800

    add feedback class into entity

---
+++ PMSDAL/Entities/FeedBack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSDAL.Entities
+{
+    public class FeedBack
+    {
+        public Guid ID { get; set; }
+        public string Creator { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string State { get; set; }
+
+        public string ProductID { get; set; }
+        public string ProductType { get; set; }
+        public string Composition { get; set; }
+        public string Customer { get; set; }
+        public string FeebackReason { get; set; }
+        public string ReceivedDate { get; set; }
+        public string ProcessWay { get; set; }
+        public string Remark { get; set; }
+    }
+}
