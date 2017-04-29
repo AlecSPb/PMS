1115d6aeb14f9f6b01d87d14b86ca3d99e7fc2a3
xs.zhou <newlifechou@outlook.com>
2017-04-29 20:16:55 +0800

    try to start use livecharts into my project

--- PMSClient/Helpers/AccessString.cs
+++ PMSClient/Helpers/AccessString.cs
@@ -12,17 +12,17 @@ namespace PMSClient.Helpers
     public static class AccessString
     {
         public static string ReadOrder { get; set; } = "浏览订单";
-        public static string EditOrder { get; set; } = "浏览订单";
-        public static string ReadOrderCheck { get; set; } = "浏览订单";
-        public static string EditOrderCheck { get; set; } = "浏览订单";
-        public static string ReadMisson { get; set; } = "浏览订单";
-        public static string ReadPlan { get; set; } = "浏览订单";
-        public static string EditPlan { get; set; } = "浏览订单";
-        public static string ReadMaterialNeed { get; set; } = "浏览订单";
-        public static string EditMaterialNeed { get; set; } = "浏览订单";
-        public static string ReadMaterialOrder { get; set; } = "浏览订单";
-        public static string EditMaterialOrder { get; set; } = "浏览订单";
-        public static string EditMaterialOrderItem { get; set; } = "浏览订单";
+        public static string EditOrder { get; set; } = "编辑订单";
+        public static string ReadOrderCheck { get; set; } = "浏览订单核验";
+        public static string EditOrderCheck { get; set; } = "编辑订单核验";
+        public static string ReadMisson { get; set; } = "浏览任务";
+        public static string ReadPlan { get; set; } = "浏览计划";
+        public static string EditPlan { get; set; } = "编辑计划";
+        public static string ReadMaterialNeed { get; set; } = "浏览材料需求";
+        public static string EditMaterialNeed { get; set; } = "编辑材料需求";
+        public static string ReadMaterialOrder { get; set; } = "浏览材料订单";
+        public static string EditMaterialOrder { get; set; } = "编辑材料订单";
+        public static string EditMaterialOrderItem { get; set; } = "编辑材料订单项";
 
 
     }
