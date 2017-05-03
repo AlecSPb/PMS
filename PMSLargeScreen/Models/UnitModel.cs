5133faef869c53b26d896dbb6d23665b339fca79
xs.zhou <newlifechou@outlook.com>
2017-05-03 11:26:26 +0800

    static data show successfully

---
+++ PMSLargeScreen/Models/UnitModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSLargeScreen.Models
+{
+    public class UnitModel
+    {
+        public UnitModel()
+        {
+            DeviceCode = "A";
+            MoldInnerDiameter = 230;
+            MoldType = "高强";
+            Pressure = 65;
+            Temp = 350;
+            Vaccum = 0.001;
+            KeepTime = 120;
+            ProcessCode = "W1";
+            FillRequirement = "BN+石墨纸+Al2O3";
+            Items = new List<UnitModelItem>()
+            {
+                new UnitModelItem {Composition="Cu22.8In20Ga7Se50.2",Quantity=1,SingleWeight=1200 },
+                new UnitModelItem {Composition="Cu22.8In21Ga6Se50.2",Quantity=2,SingleWeight=1300 },
+                new UnitModelItem {Composition="Cu22.8In22Ga5Se50.2",Quantity=3,SingleWeight=1400 }
+            };
+        }
+        public string DeviceCode { get; set; }
+        public double MoldInnerDiameter { get; set; }
+        public string MoldType { get; set; }
+        public double Pressure { get; set; }
+        public double Temp { get; set; }
+        public double Vaccum { get; set; }
+        public double KeepTime { get; set; }
+        public string ProcessCode { get; set; }
+        public string FillRequirement { get; set; }
+        public List<UnitModelItem> Items { get; set; }
+    }
+    public class UnitModelItem
+    {
+        public string Composition { get; set; }
+        public double SingleWeight { get; set; }
+        public int Quantity { get; set; }
+    }
+
+}
