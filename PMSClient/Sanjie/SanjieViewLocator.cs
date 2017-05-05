4194d26530a32ccf427a49a13a243c01ad354391
xs.zhou <newlifechou@outlook.com>
2017-05-06 07:44:12 +0800

    modify sanjie locator

--- PMSClient/Sanjie/SanjieViewLocator.cs
+++ PMSClient/Sanjie/SanjieViewLocator.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
 namespace PMSClient.Sanjie
 {
-    class SanjieViewLocator
+    public class SanjieViewLocator
     {
+        public SanjieViewLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+        }
+
+
+
+
+
     }
 }
--- PMSClient/Sanjie/SanjieViewModelLocator.cs
+++ PMSClient/Sanjie/SanjieViewModelLocator.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
 namespace PMSClient.Sanjie
 {
-    class SanjieViewModelLocator
+    public class SanjieViewModelLocator
     {
+        public SanjieViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+        }
+
+
+
     }
 }
