5614ff0cbfbb0547883c364f82cd2fd0c7b8478d
xs.zhou <newlifechou@outlook.com>
2017-05-07 08:09:36 +0800

    modify iusersimpleservice

---
+++ PMSWCFService/ServiceContracts/IUserSimpleService.cs
@@ -0,0 +1,24 @@
+﻿using PMSWCFService.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace PMSWCFService.ServiceContracts
+{
+    /// <summary>
+    /// 简单只读的用户信息服务
+    /// 用于普通类型客户端中
+    /// </summary>
+    [ServiceContract]
+    public interface IUserSimpleService
+    {
+        [OperationContract]
+        DcUser GetUser(string username, string password);
+        [OperationContract]
+        DcUserRole GetRole(Guid roleId);
+        [OperationContract]
+        List<DcUserAccess> GetAccesses(Guid roleId);
+    }
+}
