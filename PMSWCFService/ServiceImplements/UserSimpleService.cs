f337534bff544d63cab7dd56f8d3f3dea603f5fd
xs.zhou <newlifechou@outlook.com>
2017-05-07 09:01:27 +0800

    implement usersimpleaccess

---
+++ PMSWCFService/ServiceImplements/UserSimpleService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PMSWCFService.DataContracts;
+using PMSWCFService.ServiceContracts;
+using AutoMapper;
+using PMSDAL;
+
+namespace PMSWCFService
+{
+    public class UserSimpleService : IUserSimpleService
+    {
+        public List<DcUserAccess> GetAccesses(Guid roleId)
+        {
+            try
+            {
+                using (var dc = new PMSDbContext())
+                {
+                    var role = dc.Roles.Where(i => i.ID == roleId).FirstOrDefault();
+                    if (role != null)
+                    {
+                        Mapper.Initialize(cfg => cfg.CreateMap<UserAccess, DcUserAccess>());
+                        return Mapper.Map<List<UserAccess>, List<DcUserAccess>>(role.UserAccesses);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw ex;
+            }
+        }
+
+        public DcUserRole GetRole(Guid roleId)
+        {
+            try
+            {
+                using (var dc = new PMSDbContext())
+                {
+                    var role = dc.Roles.Where(i => i.ID == roleId).FirstOrDefault();
+                    Mapper.Initialize(cfg => cfg.CreateMap<UserRole, DcUserRole>());
+                    return Mapper.Map<DcUserRole>(role);
+                }
+            }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw ex;
+            }
+        }
+
+        public DcUser GetUser(string username, string password)
+        {
+            try
+            {
+                using (var dc = new PMSDbContext())
+                {
+                    Mapper.Initialize(cfg => cfg.CreateMap<User, DcUser>());
+                    var user = dc.Users.Where(i => i.UserName == username
+                    && i.Password == password
+                    && i.State == PMSCommon.UserState.雇佣.ToString()).FirstOrDefault();
+                    return Mapper.Map<DcUser>(user);
+                }
+            }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw ex;
+            }
+        }
+    }
+}
