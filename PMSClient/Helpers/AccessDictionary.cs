d1851778957db26770311978cbf385019c478aae
xs.zhou <newlifechou@outlook.com>
2017-05-09 04:03:31 +0800

    change text of accessdictionary

--- PMSClient/Helpers/AccessDictionary.cs
+++ PMSClient/Helpers/AccessDictionary.cs
@@ -16,11 +16,11 @@ namespace PMSClient
         public static string ReadOrderCheck { get; set; } = "浏览订单核验";
         public static string EditOrderCheck { get; set; } = "编辑订单核验";
 
-        public static string ReadCustomer { get; set; } = "浏览材料需求";
-        public static string EditCustomer { get; set; } = "编辑材料需求";
+        public static string ReadCustomer { get; set; } = "浏览客户信息";
+        public static string EditCustomer { get; set; } = "编辑客户信息";
 
-        public static string ReadOutSource{ get; set; } = "浏览材料需求";
-        public static string EditOutSource { get; set; } = "编辑材料需求";
+        public static string ReadOutSource{ get; set; } = "浏览外包订单";
+        public static string EditOutSource { get; set; } = "编辑外包订单";
 
         public static string ReadMaterialNeed { get; set; } = "浏览材料需求";
         public static string EditMaterialNeed { get; set; } = "编辑材料需求";
@@ -35,8 +35,8 @@ namespace PMSClient
         public static string ReadMaterialInventoryOut { get; set; } = "浏览材料出库";
         public static string EditMaterialInventoryOut { get; set; } = "编辑材料出库";
 
-        public static string ReadPlate { get; set; } = "浏览材料需求";
-        public static string EditPlate { get; set; } = "编辑材料需求";
+        public static string ReadPlate { get; set; } = "浏览背板库存";
+        public static string EditPlate { get; set; } = "编辑背板库存";
 
         public static string ReadMisson { get; set; } = "浏览任务";
         public static string ReadPlan { get; set; } = "浏览计划";
@@ -46,27 +46,27 @@ namespace PMSClient
         public static string ReadMillingRecord { get; set; } = "浏览制粉记录";
         public static string EditMillingRecord { get; set; } = "编辑制粉记录";
 
-        public static string ReadVHPRecord { get; set; } = "浏览制粉记录";
-        public static string EditVHPRecord { get; set; } = "编辑制粉记录";
+        public static string ReadVHPRecord { get; set; } = "浏览热压记录";
+        public static string EditVHPRecord { get; set; } = "编辑热压记录";
 
-        public static string ReadMachineRecord { get; set; } = "浏览制粉记录";
-        public static string EditMachineRecord { get; set; } = "编辑制粉记录";
+        public static string ReadMachineRecord { get; set; } = "浏览加工记录";
+        public static string EditMachineRecord { get; set; } = "编辑加工记录";
 
-        public static string ReadDeMoldRecord { get; set; } = "浏览制粉记录";
-        public static string EditDeMoldRecord { get; set; } = "编辑制粉记录";
+        public static string ReadDeMoldRecord { get; set; } = "浏览取模记录";
+        public static string EditDeMoldRecord { get; set; } = "编辑取模记录";
 
-        public static string ReadTestRecord { get; set; } = "浏览制粉记录";
-        public static string EditTestRecord { get; set; } = "编辑制粉记录";
+        public static string ReadTestRecord { get; set; } = "浏览测试记录";
+        public static string EditTestRecord { get; set; } = "编辑测试记录";
 
-        public static string ReadBondingRecord { get; set; } = "浏览制粉记录";
-        public static string EditBondingRecord { get; set; } = "编辑制粉记录";
+        public static string ReadBondingRecord { get; set; } = "浏览绑定记录";
+        public static string EditBondingRecord { get; set; } = "编辑绑定记录";
 
-        public static string ReadProduct { get; set; } = "浏览制粉记录";
-        public static string EditProduct { get; set; } = "编辑制粉记录";
+        public static string ReadProduct { get; set; } = "浏览产品记录";
+        public static string EditProduct { get; set; } = "编辑产品记录";
 
-        public static string ReadDelivery { get; set; } = "浏览制粉记录";
-        public static string EditDelivery { get; set; } = "编辑制粉记录";
-        public static string EditDeliveryItem { get; set; } = "编辑制粉记录";
+        public static string ReadDelivery { get; set; } = "浏览发货记录";
+        public static string EditDelivery { get; set; } = "编辑发货记录";
+        public static string EditDeliveryItem { get; set; } = "编辑发货项记录";
 
         public static string ReadStatisticOrder { get; set; } = "浏览订单统计";
         public static string ReadStatisticPlan { get; set; } = "浏览计划统计";
@@ -74,8 +74,8 @@ namespace PMSClient
         public static string ReadStatisticProduct { get; set; } = "浏览产品统计";
 
 
-        public static string ReadFeedback{ get; set; } = "浏览制粉记录";
-        public static string EditFeedback { get; set; } = "编辑制粉记录";
+        public static string ReadFeedback{ get; set; } = "浏览客户反馈";
+        public static string EditFeedback { get; set; } = "编辑客户反馈";
 
 
 
