5f36073c577357b652806dbca61e4c357087c7f2
xs.zhou <newlifechou@outlook.com>
2017-05-09 13:18:06 +0800

    implement largescreen without timer

--- PMSLargeScreenBonding/MainWindow.xaml.cs
+++ PMSLargeScreenBonding/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PMSLargeScreenBonding
         {
             InitializeComponent();
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
--- PMSLargeScreenBonding/MainWindowVM.cs
+++ PMSLargeScreenBonding/MainWindowVM.cs
@@ -5,8 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
-
-
+using System.Collections.ObjectModel;
+using PMSLargeScreenBonding.LargeScreenService;
 
 namespace PMSLargeScreenBonding
 {
@@ -16,7 +16,22 @@ namespace PMSLargeScreenBonding
         {
             currentDate = DateTime.Now;
             finishedCount = 1000;
+            status1 = status2 = status3 = "状态栏";
+            RecordBondings = new ObservableCollection<DcRecordBonding>();
+            using (var service = new LargeScreenServiceClient())
+            {
+                var result = service.GetBondingUnComplete();
+                RecordBondings.Clear();
+                result.ToList().ForEach(i => RecordBondings.Add(i));
+            }
         }
+
+
+
+
+
+        public ObservableCollection<DcRecordBonding> RecordBondings { get; set; }
+
 
         private DateTime currentDate;
 
@@ -34,7 +49,26 @@ namespace PMSLargeScreenBonding
             set { finishedCount = value; RaisePropertyChanged(nameof(FinishedCount)); }
         }
 
+        private string status1;
+        public string Status1
+        {
+            get { return status1; }
+            set { status1 = value; RaisePropertyChanged(nameof(Status1)); }
+        }
 
+        private string status2;
+        public string Status2
+        {
+            get { return status2; }
+            set { status2 = value; RaisePropertyChanged(nameof(Status2)); }
+        }
+
+        private string status3;
+        public string Status3
+        {
+            get { return status3; }
+            set { status3 = value; RaisePropertyChanged(nameof(Status3)); }
+        }
 
     }
 }
