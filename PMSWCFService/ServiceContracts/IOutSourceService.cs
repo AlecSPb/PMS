6a40b23685452cc56ec8d969405706f5b62dc7b1
xs.zhou <newlifechou@outlook.com>
2017-05-10 11:51:23 +0800

    finish outsource service

---
+++ PMSWCFService/ServiceContracts/IOutSourceService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMSDAL;
+using PMSWCFService.DataContracts;
+using System.ServiceModel;
+
+namespace PMSWCFService.ServiceContracts
+{
+    [ServiceContract]
+    public interface IOutSourceService
+    {
+        [OperationContract]
+        List<DcOutSource> GetOutSources(int s, int t, string ordername, string supplier);
+        [OperationContract]
+        int GetOutSourcesCount(string ordername, string supplier);
+
+        [OperationContract]
+        int AddOutSource(DcOutSource model,string uid);
+        [OperationContract]
+        int UpdateOutSource(DcOutSource model,string uid);
+        [OperationContract]
+        int DeleteOutSource(Guid id,string uid);
+    }
+}
---
+++ PMSWCFService/ServiceImplements/OutSourceService.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PMSDAL;
+using PMSWCFService.DataContracts;
+using PMSWCFService.ServiceContracts;
+using AutoMapper;
+
+namespace PMSWCFService
+{
+    public partial class PMSService : IOutSourceService
+    {
+        public int AddOutSource(DcOutSource model, string uid)
+        {
+            try
+            {
+                using (var dc = new PMSDbContext())
+                {
+                    Mapper.Initialize(cfg => cfg.CreateMap<DcOutSource, OutSource>());
+                    var entity = Mapper.Map<OutSource>(model);
+                    dc.OutSources.Add(entity);
+                    SaveHistory(model, uid);
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw;
+            }
+        }
+
+        public int DeleteOutSource(Guid id, string uid)
+        {
+            try
+            {
+                using (var dc = new PMSDbContext())
+                {
+                    var entity = dc.OutSources.Find(id);
+                    dc.OutSources.Remove(entity);
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw;
+            }
+        }
+
+        public List<DcOutSource> GetOutSources(int s, int t, string ordername, string supplier)
+        {
+            try
+            {
+                using (var dc = new PMSDbContext())
+                {
+                    Mapper.Initialize(cfg => cfg.CreateMap<OutSource, DcOutSource>());
+                    var query = from i in dc.OutSources
+                                where i.State != PMSCommon.OrderState.作废.ToString()
+                                && i.OrderName.Contains(ordername)
+                                && i.Supplier.Contains(supplier)
+                                orderby i.CreateTime descending
+                                select i;
+                    return Mapper.Map<List<OutSource>, List<DcOutSource>>(query.Skip(s).Take(t).ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw;
+            }
+        }
+
+        public int GetOutSourcesCount(string ordername, string supplier)
+        {
+            try
+            {
+                using (var dc = new PMSDbContext())
+                {
+                    var query = from i in dc.OutSources
+                                where i.State != PMSCommon.OrderState.作废.ToString()
+                                && i.OrderName.Contains(ordername)
+                                && i.Supplier.Contains(supplier)
+                                select i;
+                    return query.Count();
+                }
+            }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw;
+            }
+        }
+
+        public int UpdateOutSource(DcOutSource model, string uid)
+        {
+            try
+            {
+                using (var dc = new PMSDbContext())
+                {
+                    Mapper.Initialize(cfg => cfg.CreateMap<DcOutSource, OutSource>());
+                    var entity = Mapper.Map<OutSource>(model);
+                    dc.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+                    SaveHistory(model, uid);
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+                throw;
+            }
+        }
+
+
+
+        private void SaveHistory(DcOutSource model, string uid)
+        {
+            try
+            {
+                using (var dc = new PMSDbContext())
+                {
+                    var config = new MapperConfiguration(cfg => cfg.CreateMap<DcOutSource, OutSourceHistory>());
+                    var mapper = config.CreateMapper();
+                    var history = mapper.Map<OutSourceHistory>(model);
+                    history.OperateTime = DateTime.Now;
+                    history.Operator = uid;
+                    history.HistoryID = Guid.NewGuid();
+                    dc.OutSourceHistorys.Add(history);
+                    dc.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                LocalService.CurrentLog.Error(ex);
+            }
+        }
+    }
+}
