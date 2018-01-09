f1f887a7ca86fd237ae1cf2d82e9bcaa6a6327cc
xs.zhou <newlifechou@outlook.com>
2018-01-09 13:05:34 +0800

    create small tools

--- PMSClient/View/ToolView.xaml.cs
+++ PMSClient/View/ToolView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PMSClient.Tool;
 
 namespace PMSClient.View
 {
@@ -29,5 +30,33 @@ namespace PMSClient.View
         {
             NavigationService.GoTo(PMSViews.Navigation);
         }
+
+        private void btnCompositionToOneWindow_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                CompositionToOne window = new CompositionToOne();
+                window.Show();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void btnPressureTransmissionWindow_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PressureChangeTool window = new PressureChangeTool();
+                window.Show();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
     }
 }
