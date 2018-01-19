e9bb66b7811488ead433ba3a38fbc79c481e6545
xs.zhou <newlifechou@outlook.com>
2018-01-19 10:48:45 +0800

    add notice to pmsdal

---
+++ PMSDAL/Entities/Notice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSDAL
+{
+    public class Notice
+    {
+        public Guid ID { get; set; }
+        public string Content { get; set; }
+        public string  Title { get; set; }
+        public string  Creator { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
--- PMSDAL/PMSDbContext.cs
+++ PMSDAL/PMSDbContext.cs
@@ -115,6 +115,6 @@ namespace PMSDAL
 
         public DbSet<EnvironmentInfo> EnvironmentInfos { get; set; }
 
-
+        public DbSet<Notice> Notices { get; set; }
     }
 }
