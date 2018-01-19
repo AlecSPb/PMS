8578b8548798f4bc0df4fec6ac4164a37f82e476
xs.zhou <newlifechou@outlook.com>
2018-01-19 10:50:32 +0800

    add dcnotice to wcfservice

---
+++ PMSWCFService/DataContracts/DcNotice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace PMSWCFService.DataContracts
+{
+    [DataContract]
+    public class DcNotice
+    {
+        [DataMember]
+        public Guid ID { get; set; }
+        [DataMember]
+        public string Content { get; set; }
+        [DataMember]
+        public string Title { get; set; }
+        [DataMember]
+        public string Creator { get; set; }
+        [DataMember]
+        public DateTime CreateTime { get; set; }
+    }
+}
