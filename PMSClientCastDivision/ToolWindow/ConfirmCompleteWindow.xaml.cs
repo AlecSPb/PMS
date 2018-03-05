197fd51febf8a107ea1da960c795da18c7c7e2b7
xs.zhou <newlifechou@outlook.com>
2018-03-05 11:16:47 +0800

    complete the confirmcompletewindow

--- PMSClientCastDivision/ToolWindow/ConfirmCompleteWindow.xaml.cs
+++ PMSClientCastDivision/ToolWindow/ConfirmCompleteWindow.xaml.cs
@@ -23,5 +23,46 @@ namespace PMSClient.ToolWindow
         {
             InitializeComponent();
         }
+
+        public ConfirmModel Model
+        {
+            get
+            {
+                ConfirmModel model = new ConfirmModel()
+                {
+                    MaterialItemLot = txtMaterialLot.Text,
+                    Composition = txtComposition.Text,
+                    PMINumber = txtPMINumber.Text,
+                    Weight = double.Parse(txtWeight.Text),
+                    ActualWeight = double.Parse(txtActualWeight.Text),
+                    MeltingPoint = txtMeltingPoint.Text,
+                    Remark=txtRemark.Text
+                };
+                return model;
+            }
+            set
+            {
+                txtMaterialLot.Text = value.MaterialItemLot;
+                txtComposition.Text = value.Composition;
+                txtPMINumber.Text = value.PMINumber;
+                txtWeight.Text = value.Weight.ToString();
+                txtActualWeight.Text = value.ActualWeight.ToString();
+                txtMeltingPoint.Text = value.MeltingPoint;
+                txtRemark.Text = value.Remark;
+            }
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void btnSure_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
     }
 }
---
+++ PMSClientCastDivision/ToolWindow/ConfirmModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSClient.ToolWindow
+{
+    public class ConfirmModel
+    {
+        public ConfirmModel()
+        {
+            MaterialItemLot = "";
+            Composition = "";
+            PMINumber = "";
+            Weight = ActualWeight = 0;
+            MeltingPoint = Remark = "";
+        }
+        public string MaterialItemLot { get; set; }
+        public string Composition { get; set; }
+        public string PMINumber { get; set; }
+        public double Weight { get; set; }
+        public double ActualWeight { get; set; }
+
+        public string MeltingPoint { get; set; }
+        public string Remark { get; set; }
+
+    }
+}
