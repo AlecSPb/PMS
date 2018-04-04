0b83cc885a683b38bc816544bc2a8fbbf97f3506
xs.zhou <newlifechou@outlook.com>
2018-04-04 10:03:23 +0800

    change the items order of plan view

--- PMSClient/MainDesktop.xaml.cs
+++ PMSClient/MainDesktop.xaml.cs
@@ -61,7 +61,7 @@ namespace PMSClient
             {
                 var titleName = (App.Current as App).FindResource("AppNameDesktop").ToString();
                 //var versonName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
-                var versonName = "4.10.21.26";
+                var versonName = "4.10.21.28";
                 Title = $"{titleName}-{versonName}";
             }
             catch (Exception)
