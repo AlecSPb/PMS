a6ab5a742d467a56ced73cfc4f961e8458b96884
xs.zhou <newlifechou@outlook.com>
2018-05-29 20:11:51 +0800

    start to create new reporthelper libs

---
+++ PMSClient/ReportsHelperNew/ReportBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSClient.ReportsHelperNew
+{
+    public class ReportBase
+    {
+        // 输出文件名
+        protected string wordFileName = string.Empty;
+        public ReportBase()
+        {
+
+        }
+
+        public void Intialize(string word2007FileName)
+        {
+
+        }
+
+
+
+
+
+
+
+    }
+}
