e005da3ce0ea4692184095e28dff36d9e5b21e09
xs.zhou <newlifechou@outlook.com>
2018-08-04 18:44:57 +0800

    add precaution measures to plan edit

---
+++ PMSClient/Helpers/VHPHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSClient.Helpers
+{
+    public static class VHPHelper
+    {
+        public static string CheckPlanTypeAndProcessCode(string planType, string processCode)
+        {
+
+            switch (processCode)
+            {
+                case "W1":
+                    if (planType == PMSCommon.VHPPlanType.回收.ToString())
+                    {
+                        return "";
+                    }
+                    else
+                    {
+                        return "工艺代码[W1]通常对应的后续工作是[回收]";
+                    }
+                case "W2":
+                case "W3":
+                case "F1":
+                    if (planType == PMSCommon.VHPPlanType.加工.ToString())
+                    {
+                        return "";
+                    }
+                    else
+                    {
+                        return "工艺代码[W2 W3 F1]通常对应的后续工作是[加工]";
+                    }
+                default:
+                    return "";
+            }
+
+        }
+    }
+}
--- PMSClient/ViewModel/PlanEditVM.cs
+++ PMSClient/ViewModel/PlanEditVM.cs
@@ -202,6 +202,30 @@ namespace PMSClient.ViewModel
                 var service = new PlanVHPServiceClient();
                 if (IsNew)
                 {
+                    //新添加-检查流程
+                    //检查是否同一天同一台设备已经安排有计划
+                    using (var check = new MissonServiceClient())
+                    {
+                        string search_code = $"{CurrentPlan.PlanDate.ToString("yyMMdd")}-{CurrentPlan.VHPDeviceCode}";
+                        var results = check.GetPlanExtra(0, 20, search_code, string.Empty);
+                        if (results.Count() > 0)
+                        {
+                            if (!PMSDialogService.ShowYesNo("确定",
+                                    $"同一天同一台设备已经安排了[{results.Count()}]个计划，\r\n仍继续添加此计划？") == true)
+                                return;
+                        }
+                    }
+                    //检查后续代码和工艺代码是否符合常规
+
+                    string check_result = Helpers.VHPHelper.CheckPlanTypeAndProcessCode(CurrentPlan.PlanType, CurrentPlan.ProcessCode);
+                    if (check_result != "")
+                    {
+                        if (!PMSDialogService.ShowYesNo("提醒", 
+                            $"{check_result},\r\n而当前工艺代码是[{CurrentPlan.ProcessCode}],热压类型是[{CurrentPlan.PlanType}],\r\n仍继续添加此计划吗？"))
+                            return;
+                    }
+
+
                     service.AddVHPPlanByUID(CurrentPlan, uid);
                 }
                 else
