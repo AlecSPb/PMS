922c5e8f9606aea6e7d08a496046ce778fbfcb42
xs.zhou <newlifechou@outlook.com>
2018-08-14 09:18:19 +0800

    start to build consumable modules

---
+++ PMSDAL/Entities/Consumable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSDAL
+{
+    public class Consumable
+    {
+        public Guid ID { get; set; }
+        public string Creator { get; set; }
+        public string CreateTime { get; set; }
+        public string State { get; set; }
+
+
+        public string ItemName { get; set; }
+        public string ItemDetails { get; set; }
+        public double Quantity { get; set; }
+        public string QuantityUnit { get; set; }
+
+        public string ForWho { get; set; }
+        public string SupplierSource { get; set; }
+
+
+        public double Cost { get; set; }
+
+        public string Remark { get; set; }
+
+
+    }
+}
