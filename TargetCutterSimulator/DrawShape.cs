0b7021138130b22ccc7d4fdb65e1a18ab9bef3be
xs.zhou <newlifechou@outlook.com>
2018-11-03 23:50:13 +0800

    add more functions to paint

---
+++ TargetCutterSimulator/DrawShape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace TargetCutterSimulator
+{
+    public enum ShapeType
+    {
+        Rectangle,
+        Ellipse
+    }
+    public class DrawShape
+    {
+        public Shape Shape { get; set; }
+        public ShapeType CurrentType { get; set; }
+    }
+}
--- TargetCutterSimulator/MainWindow.xaml.cs
+++ TargetCutterSimulator/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using IO = System.IO;
 
 namespace TargetCutterSimulator
 {
@@ -26,7 +27,6 @@ namespace TargetCutterSimulator
             Initialize();
         }
 
-        private List<Shape> shapeList = new List<Shape>();
 
         private void Initialize()
         {
@@ -34,6 +34,8 @@ namespace TargetCutterSimulator
         }
 
         private const double ratio = 2.0;
+        private const double normal_thickness = 3.0;
+        private const double select_thickness = 4.0;
 
         private void AddEllipse(double diameter)
         {
@@ -41,7 +43,7 @@ namespace TargetCutterSimulator
             circle.Height = diameter * ratio;
             circle.Width = diameter * ratio;
             circle.Fill = Brushes.Transparent;
-            circle.StrokeThickness = 1;
+            circle.StrokeThickness = normal_thickness;
             circle.Stroke = Brushes.Blue;
             circle.MouseDown += Shape_MouseDown;
             circle.MouseMove += Shape_MouseMove;
@@ -61,7 +63,7 @@ namespace TargetCutterSimulator
             rect.Height = height * ratio;
             rect.Width = width * ratio;
             rect.Fill = Brushes.Transparent;
-            rect.StrokeThickness = 1;
+            rect.StrokeThickness = normal_thickness;
             rect.Stroke = Brushes.Blue;
             rect.MouseDown += Shape_MouseDown;
             rect.MouseMove += Shape_MouseMove;
@@ -77,10 +79,11 @@ namespace TargetCutterSimulator
 
         private void Shape_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            if (e.ButtonState == MouseButtonState.Pressed)
             {
                 currentShape = e.OriginalSource as Shape;
                 previousPoint = e.GetPosition(currentShape);
+                currentShape.StrokeThickness = select_thickness;
             }
         }
 
@@ -103,17 +106,113 @@ namespace TargetCutterSimulator
 
         private void BtnAddCircle_Click(object sender, RoutedEventArgs e)
         {
-            AddEllipse(50.8);
+
+            if (double.TryParse(TxtDiameter.Text.Trim(), out double diameter)
+                            && diameter > 0)
+            {
+                AddEllipse(diameter);
+            }
         }
 
         private void BtnAddRectangle_Click(object sender, RoutedEventArgs e)
         {
-            AddRectangle(100, 50);
+            bool result1 = double.TryParse(TxtWidth.Text.Trim(), out double width);
+            bool result2 = double.TryParse(TxtHeight.Text.Trim(), out double height);
+            if (result1 && result2 && width > 0 && height > 0)
+            {
+                AddRectangle(width, height);
+
+            }
         }
 
         private void Shape_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            currentShape = null;
+            if (currentShape != null)
+            {
+                currentShape.StrokeThickness = normal_thickness;
+
+            }
+            //currentShape = null;
+            System.Diagnostics.Debug.WriteLine("Up");
+        }
+
+        private void CmCopy_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentShape != null)
+            {
+                if (currentShape.GetType() == typeof(Rectangle))
+                {
+                    AddRectangle(currentShape.Width / ratio, currentShape.Height / ratio);
+                }
+                else if (currentShape.GetType() == typeof(Ellipse))
+                {
+                    AddEllipse(currentShape.Width / ratio);
+                }
+            }
+            e.Handled = true;
+        }
+
+        private void CmDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentShape != null)
+            {
+                MainCanvas.Children.Remove(currentShape);
+                currentShape = null;
+            }
+            e.Handled = true;
+        }
+
+        private string targetFolder = IO.Path.Combine(Environment.CurrentDirectory, "SaveAs");
+        private void BtnSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Size size = new Size(this.Width, this.Height);
+                MainCanvas.Measure(size);
+
+                var rtb = new RenderTargetBitmap(
+                    (int)MainCanvas.ActualWidth,
+                    (int)MainCanvas.ActualHeight,
+                    96,
+                    96,
+                    PixelFormats.Pbgra32
+                    );
+
+                rtb.Render(MainCanvas);
+
+                var enc = new System.Windows.Media.Imaging.PngBitmapEncoder();
+                enc.Frames.Add(System.Windows.Media.Imaging.BitmapFrame.Create(rtb));
+
+                string fileName = IO.Path.Combine(targetFolder, $"TC_{DateTime.Now.ToString("yyyyMMddHHmmss")}.png");
+                using (var stm = IO.File.Create(fileName))
+                {
+                    enc.Save(stm);
+                }
+
+                MessageBox.Show("保存成功");
+                System.Diagnostics.Process.Start(targetFolder);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnOpenSaveFolder_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(targetFolder);
+        }
+
+        private void BtnClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("清除画布上所有的形状?", "请问", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                if (MainCanvas.Children.Count > 0)
+                {
+                    MainCanvas.Children.Clear();
+                }
+            }
+
         }
     }
 }
