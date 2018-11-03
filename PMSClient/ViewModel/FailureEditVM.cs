c108da00c3d03c70554cb6f314e71c14a30ca89c
xs.zhou <newlifechou@outlook.com>
2018-11-03 15:20:24 +0800

    try to move shape and ok

--- PMSClient/ViewModel/FailureEditVM.cs
+++ PMSClient/ViewModel/FailureEditVM.cs
@@ -8,5 +8,6 @@ namespace PMSClient.ViewModel
 {
     public class FailureEditVM
     {
+
     }
 }
--- TargetCutterSimulator/MainWindow.xaml.cs
+++ TargetCutterSimulator/MainWindow.xaml.cs
@@ -23,33 +23,97 @@ namespace TargetCutterSimulator
         public MainWindow()
         {
             InitializeComponent();
+            Initialize();
         }
+
+        private List<Shape> shapeList = new List<Shape>();
+
+        private void Initialize()
+        {
+            this.Title = "靶材切割设计器";
+        }
+
+        private const double ratio = 2.0;
+
+        private void AddEllipse(double diameter)
+        {
+            Ellipse circle = new Ellipse();
+            circle.Height = diameter * ratio;
+            circle.Width = diameter * ratio;
+            circle.Fill = Brushes.Transparent;
+            circle.StrokeThickness = 1;
+            circle.Stroke = Brushes.Blue;
+            circle.MouseDown += Shape_MouseDown;
+            circle.MouseMove += Shape_MouseMove;
+            circle.MouseUp += Shape_MouseUp;
+
+            TransformGroup transformGroup = new TransformGroup();
+            transformGroup.Children.Add(new RotateTransform());
+            transformGroup.Children.Add(new TranslateTransform());
+            circle.RenderTransform = transformGroup;
+
+            MainCanvas.Children.Add(circle);
+        }
+
+        private void AddRectangle(double width, double height)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Height = height * ratio;
+            rect.Width = width * ratio;
+            rect.Fill = Brushes.Transparent;
+            rect.StrokeThickness = 1;
+            rect.Stroke = Brushes.Blue;
+            rect.MouseDown += Shape_MouseDown;
+            rect.MouseMove += Shape_MouseMove;
+            rect.MouseUp += Shape_MouseUp;
+
+            TransformGroup transformGroup = new TransformGroup();
+            transformGroup.Children.Add(new RotateTransform());
+            transformGroup.Children.Add(new TranslateTransform());
+            rect.RenderTransform = transformGroup;
+
+            MainCanvas.Children.Add(rect);
+        }
+
+        private void Shape_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                currentShape = e.OriginalSource as Shape;
+                previousPoint = e.GetPosition(currentShape);
+            }
+        }
+
         private Point previousPoint = new Point(0, 0);
-        private void Shape1_MouseDown(object sender, MouseButtonEventArgs e)
+        private Shape currentShape = null;
+
+        private void Shape_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            if (e.LeftButton == MouseButtonState.Pressed && currentShape != null)
             {
-                previousPoint = e.GetPosition(MainCanvas);
+                Point position = e.GetPosition(currentShape);
+
+                TranslateTransform translate = (currentShape.RenderTransform as TransformGroup)
+                                                        .Children[1] as TranslateTransform;
+                translate.X += position.X - this.previousPoint.X;
+                translate.Y += position.Y - this.previousPoint.Y;
+                System.Diagnostics.Debug.WriteLine($"{position.X},{position.Y}");
             }
         }
 
-        private void Shape1_MouseMove(object sender, MouseEventArgs e)
+        private void BtnAddCircle_Click(object sender, RoutedEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                Point position = e.GetPosition(MainCanvas);
-
-                tlt.X = position.X - this.previousPoint.X;
-                tlt.Y = position.Y - this.previousPoint.Y;
-            }
+            AddEllipse(50.8);
         }
 
-        private void Shape1_MouseLeave(object sender, MouseEventArgs e)
+        private void BtnAddRectangle_Click(object sender, RoutedEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                previousPoint = new Point(tlt.X, tlt.Y);
-            }
+            AddRectangle(100, 50);
+        }
+
+        private void Shape_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            currentShape = null;
         }
     }
 }
