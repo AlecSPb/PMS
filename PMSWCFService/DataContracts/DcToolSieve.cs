14fa27c8142ad1066219d3b017a51416291a0800
xs.zhou <newlifechou@outlook.com>
2020-02-14 09:07:59 +0800

    add toolsieve service to wcf service

--- PMSWCFService/DataContracts/DcToolSieve.cs
+++ PMSWCFService/DataContracts/DcToolSieve.cs
@@ -1,25 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PMSDAL
+namespace PMSWCFService.DataContracts
 {
-    /// <summary>
-    /// 筛网库
-    /// </summary>
-    public class ToolSieve:ModelBase
+    [DataContract]
+    public class DcToolSieve
     {
+        [DataMember]
+        public Guid Id { get; set; }
+        [DataMember]
+        public DateTime CreateTime { get; set; }
+        [DataMember]
+        public string Creator { get; set; }
+
+        [DataMember]
+        public string State { get; set; }
+
+
+        [DataMember]
         public string SearchID { get; set; }//给人看的编号  S-1
+        [DataMember]
         public string Manufacture { get; set; }//制造商
+        [DataMember]
         public string Specification { get; set; }//规格
+        [DataMember]
         public string MaterialGroup { get; set; }//材料组
+        [DataMember]
         public string Remark { get; set; }
 
+        [DataMember]
         public DateTime StartTime { get; set; }//开始使用时间
+        [DataMember]
         public DateTime StopTime { get; set; }//结束使用时间
-
-        //State:CommonToolState
     }
 }
---
+++ PMSWCFService/ServiceContracts/IToolSieveService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using PMSDAL;
+using PMSWCFService.DataContracts;
+
+namespace PMSWCFService.ServiceContracts
+{
+    [ServiceContract]
+    public interface IToolSieveService
+    {
+        List<DcToolSieve> GetToolSieve();
+        int GetToolSieveCount();
+
+        void AddToolSieve(ToolSieve model);
+        void UpdateToolSieve(ToolSieve model);
+    }
+}
