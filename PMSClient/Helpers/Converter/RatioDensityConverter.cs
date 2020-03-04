3ae48edf8ac25afd3dadce98d4f1d7936c5d4803
xs.zhou <newlifechou@outlook.com>
2020-03-04 10:11:32 +0800

    add color indicator to density ratio

---
+++ PMSClient/Helpers/Converter/RatioDensityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PMSClient.Helpers.Converter
+{
+    public class RatioDensityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double threshold = (double)value;
+            if (threshold < 0.9)
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Green);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
