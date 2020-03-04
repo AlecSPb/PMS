691d95d2146ec455e4a8109edf600bb02cc67404
xs.zhou <newlifechou@outlook.com>
2020-03-04 14:10:02 +0800

    add more details

---
+++ PMSClient/Helpers/Converter/CurrentVHPDateConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PMSClient.Helpers.Converter
+{
+    public class CurrentVHPDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime plandate = (DateTime)value;
+            if (plandate.Date == DateTime.Today)
+            {
+                return new SolidColorBrush(Colors.Green);
+            }
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
