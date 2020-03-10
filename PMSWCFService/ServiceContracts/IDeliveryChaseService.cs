8d551cf2b204c5e40848eb485729c6fe0195bd36
xs.zhou <newlifechou@outlook.com>
2020-03-11 02:15:41 +0800

    finish deliverychase contract

--- PMSWCFService/ServiceContracts/IDeliveryChaseService.cs
+++ PMSWCFService/ServiceContracts/IDeliveryChaseService.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using PMSDAL;
+using PMSWCFService.DataContracts;
 
 namespace PMSWCFService.ServiceContracts
 {
-    interface IDeliveryChaseService
+    [ServiceContract]
+    public interface IDeliveryChaseService
     {
+        [OperationContract]
+        List<DcDeliveryChase> GetDeliveryChaseAll(int s, int t, string lot, string composition);
+
+        [OperationContract]
+        int GetDeliveryChaseAllCount(string lot, string composition);
+
+
+        [OperationContract]
+        List<DcDeliveryChase> GetDeliveryChase(int s, int t, string lot, string composition);
+
+        [OperationContract]
+        int GetDeliveryChaseCount(string lot, string composition);
+
+        [OperationContract]
+        void AddDeliveryChaseCount(DcDeliveryChase model);
+        [OperationContract]
+        void UpdateDeliveryChaseCount(DcDeliveryChase model);
     }
 }
