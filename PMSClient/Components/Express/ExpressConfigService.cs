f89389930be19c1df2150d6860931e167e6b7c97
xs.zhou <newlifechou@outlook.com>
2020-03-18 13:22:56 +0800

    change some config to database

---
+++ PMSClient/Components/Express/ExpressConfigService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMSClient.ExtraService;
+
+namespace PMSClient.Components.Express
+{
+
+    public class ExpressConfigService
+    {
+        public static string ReadKey(string key)
+        {
+			try
+			{
+				using (var s=new )
+				{
+
+				}
+			}
+			catch (Exception)
+			{
+                return "";
+			}
+        }
+    }
+}
