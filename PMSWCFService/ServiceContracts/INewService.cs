21f1299484d63900e3470c8194de7e8e40c08f77
xs.zhou <newlifechou@outlook.com>
2020-03-20 20:34:00 +0800

    add new service contract into wcf service

---
+++ PMSWCFService/ServiceContracts/INewService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using PMSWCFService.DataContracts;
+
+namespace PMSWCFService.ServiceContracts
+{
+    /// <summary>
+    /// 这个是尝试新功能的新服务
+    /// 旧的服务接口不再变动，保持兼容
+    /// 新服务另起炉灶
+    /// </summary>
+    [ServiceContract]
+    public interface INewService
+    {
+        #region 订单
+        [OperationContract]
+        List<DcOrder> GetOrder(int s, int t, string customer, string composition, string pminumber, string state);
+        [OperationContract]
+        int GetOrderCount(string customer, string composition, string pminumber, string state);
+
+        [OperationContract]
+        DateTime GetOrderLastUpdateTime(Guid id);
+        #endregion
+
+        #region 任务
+        [OperationContract]
+        List<DcOrder> GetMisson(int s, int t, string composition, string pminumber, string state);
+        [OperationContract]
+        int GetMissonCount(string composition, string pminumber, string state);
+
+        #endregion
+
+        #region 测试
+        [OperationContract]
+        DateTime GetRecordTestLastUpdateTime(Guid id);
+        #endregion
+
+
+    }
+}
---
+++ PMSWCFService/ServiceImplements/NewService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using PMSWCFService.DataContracts;
+using PMSWCFService.ServiceContracts;
+using PMSDAL;
+
+namespace PMSWCFService
+{
+    public class NewService : INewService
+    {
+        public List<DcOrder> GetMisson(int s, int t, string composition, string pminumber, string state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetMissonCount(string composition, string pminumber, string state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DcOrder> GetOrder(int s, int t, string customer, string composition, string pminumber, string state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetOrderCount(string customer, string composition, string pminumber, string state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetOrderLastUpdateTime(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetRecordTestLastUpdateTime(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
