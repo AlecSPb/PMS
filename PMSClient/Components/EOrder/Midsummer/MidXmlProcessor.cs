3a92421e8affb6123abb5361223d50caa6943c73
xs.zhou <newlifechou@outlook.com>
2020-10-21 11:52:35 +0800

    start to add midsummer xml processor to pms

---
+++ PMSClient/Components/EOrder/Midsummer/MidXmlProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSClient.Components.EOrder.Midsummer
+{
+    /// <summary>
+    /// 用来处理Midsummer的Xml订单文件
+    /// </summary>
+    public class MidXmlProcessor
+    {
+
+    }
+}
