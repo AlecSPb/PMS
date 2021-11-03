2299771d177c17e4c7d1b05200584421ec4fc5ec
xs.zhou <newlifechou@outlook.com>
2021-11-04 06:41:43 +0800

    add SelectDocTypeDialog

---
+++ PMSQuotation/Docs/DocService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMSQuotation.Models;
+using Xceed.Words;
+
+namespace PMSQuotation.Docs
+{
+    public class DocService
+    {
+        public DocService()
+        {
+
+        }
+        private string targetFile;
+        private string sourceFile;
+        private string tempFile;
+
+        public void GenerateChinese()
+        {
+
+        }
+
+        public void GenerateEnglish()
+        {
+
+        }
+
+    }
+}
