ef910e41e3801b9be637e211d5df3343176e5dc6
xs.zhou <xs.zhou@outlook.com>
2021-11-07 11:20:55 +0800

    add json file function,implement later

---
+++ PMSQuotation/Models/QuotationJsonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSQuotation.Models
+{
+    /// <summary>
+    /// 用于json文件导入导出
+    /// </summary>
+    public class QuotationJsonModel
+    {
+        public Quotation Quotation { get; set; }
+        public List<QuotationItem> Items { get; set; }
+    }
+}
